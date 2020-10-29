4546014ba027bc253a0e58e48df6f49fc9c8b5ef
Aidan B <aidanrichbennett+github@gmail.com>
2020-10-28 23:57:23 -0400

    Custom button text/CSS

--- script.js
+++ script.js
@@ -5,14 +5,27 @@ var generate = function() {
     let amount = document.getElementsByName('amount')[0].value
     let return_ = document.getElementsByName('return')[0].value
     let cancel_return = document.getElementsByName('cancel_return')[0].value
-    let output = `<FORM ACTION="https://www.paypal.com/cgi-bin/webscr" METHOD="POST" target="_blank">
-    <INPUT TYPE="hidden" NAME="cmd" VALUE="`+command+`">
-    <INPUT TYPE="hidden" NAME="business" VALUE="`+business+`">
-    <INPUT TYPE="hidden" NAME="amount" VALUE="`+amount+`">
-    <INPUT TYPE="hidden" NAME="item_name" VALUE="`+item_name+`">
-    <INPUT TYPE="hidden" NAME="return" VALUE="`+return_+`">
-    <INPUT TYPE="hidden" NAME="cancel_return" VALUE="`+cancel_return+`">
-    <INPUT class="sponsor" TYPE="image" SRC="https://mz-ot.github.io/paypalbutton/sharethecost.png" BORDER="0" NAME="submit" ALT="`+item_name+`">
+    let button_text = document.getElementsByName('button_text')[0].value
+    let output = `<style>
+button {
+    padding:  10px 20px;
+    border-radius: 4px;
+    border: 1px solid black;
+    background: #82bde0;
+    transition: all .3s;
+    cursor: pointer;
+}
+
+button:hover {box-shadow: 0 2px 4px black;}
+</style>    
+<FORM ACTION="https://www.paypal.com/cgi-bin/webscr" METHOD="POST" target="_blank">
+    <INPUT TYPE="hidden" NAME="cmd" VALUE="` + command + `">
+    <INPUT TYPE="hidden" NAME="business" VALUE="` + business + `">
+    <INPUT TYPE="hidden" NAME="amount" VALUE="` + amount + `">
+    <INPUT TYPE="hidden" NAME="item_name" VALUE="` + item_name + `">
+    <INPUT TYPE="hidden" NAME="return" VALUE="` + return_ + `">
+    <INPUT TYPE="hidden" NAME="cancel_return" VALUE="` + cancel_return + `">
+    <button>` + button_text + `</button>
 </FORM>`
     document.getElementById('output').value = output
     document.getElementById('label').innerHTML = 'Click below to copy to clipboard'
